b2eab5a1c57d989278931d7c276c985abae1369a
Frank <frankg2694@hotmail.com>
2024-02-10 16:23:03 -0700

    Added React Build

--- server/server.js
+++ server/server.js
@@ -3,20 +3,17 @@ const app = express();
 const { getPokemon } = require('./getPokemon.js')
 const port = process.env.PORT || 8080;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from Express!');
-// });
+app.use(express.static('../client/build'));
 
 app.get('/pokemonSearch', async (req, res, next) => {
   try {
     const pokemon = await getPokemon();
     res.json(pokemon);
   } catch (error) {
-    // Passes errors into the error handler
     return next(error)
   }
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
